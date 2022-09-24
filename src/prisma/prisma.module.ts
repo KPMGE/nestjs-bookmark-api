0b0e0075b40e91ac76f83f57b37579d96d554287
KPMGE <jesuskevin254@gmail.com>
2022-09-24 15:34:00 -0300

    :recycle: set prisma module as global

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,6 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
+// everything that is in the exports array will be available throughout the 
+// whole application without needing for import
+@Global()
 @Module({
   providers: [PrismaService],
   exports: [PrismaService]
