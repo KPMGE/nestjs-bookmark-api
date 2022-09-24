aab26d69dc8bf8ce11450774ee2642d901be9032
KPMGE <jesuskevin254@gmail.com>
2022-09-24 15:32:02 -0300

    :sparkles: add prisma service to auth module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class AuthService {
+  constructor(private prismaService: PrismaService) { }
+
   signin() {
     return { data: "i'm signed in" }
   }
