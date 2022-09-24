8729d978e6496f180b116507fc09a78f12ea5600
KPMGE <jesuskevin254@gmail.com>
2022-09-24 15:51:30 -0300

    :sparkles: implement signin feature

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
+import { hash } from 'argon2'
 
 @Injectable()
 export class AuthService {
   constructor(private prismaService: PrismaService) { }
 
-  signin(dto: AuthDto) {
-    return { data: "i'm signed in" }
+  async signin(dto: AuthDto) {
+    const passwordHash = await hash(dto.password)
+    const user = await this.prismaService.user.create({
+      data: {
+        email: dto.email,
+        passwordHash
+      }
+    })
+
+    delete user.passwordHash
+
+    return user
   }
 
   signup() {
