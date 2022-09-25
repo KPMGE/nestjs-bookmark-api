0644bc6fef7e29752a0f1d91678a78d4cabd5d68
KPMGE <jesuskevin254@gmail.com>
2022-09-25 10:44:58 -0300

    :recycle: create a custom jwt guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express'
+import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users')
 export class UserController {
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtGuard)
   @Get('me')
   getMe(@Req() req: Request) {
     return req.user;
