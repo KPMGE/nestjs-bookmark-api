c399199a6059ab40845e34769a761f2e4351af99
KPMGE <jesuskevin254@gmail.com>
2022-09-25 09:57:18 -0300

    :sparkles: set up environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,18 @@ import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true
+    })
+  ],
   providers: [PrismaService],
 })
 export class AppModule { }
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(private configService: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgresql://postgres:1234@localhost:5432/bookmark?schema=public'
+          url: configService.get('DATABASE_URL')
         }
       }
     })
